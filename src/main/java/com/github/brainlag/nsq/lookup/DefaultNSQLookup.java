fcb7986220ed83beff690e8185e4c49cedcc40c5
Matt Guindin <mguindin@movableink.com>
2015-10-01 16:14:34 -0400

    Adding exception handling/throwing for unreachable nsqdlookup servers

--- src/main/java/com/github/brainlag/nsq/lookup/DefaultNSQLookup.java
+++ src/main/java/com/github/brainlag/nsq/lookup/DefaultNSQLookup.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
 public class DefaultNSQLookup implements NSQLookup {
     Set<String> addresses = Sets.newHashSet();
 
@@ -26,16 +27,29 @@ public class DefaultNSQLookup implements NSQLookup {
         Set<ServerAddress> addresses = Sets.newHashSet();
 
         for (String addr : this.addresses) {
-            ObjectMapper mapper = new ObjectMapper();
+            try {
+                ObjectMapper mapper = new ObjectMapper();
 
-            JsonNode jsonNode = mapper.readTree(new URL(addr + "/lookup?topic=" + topic));
-            JsonNode producers = jsonNode.get("data").get("producers");
-            for (JsonNode node : producers) {
-                String host = node.get("broadcast_address").asText();
-                ServerAddress address = new ServerAddress(host, node.get("tcp_port").asInt());
-                addresses.add(address);
+                JsonNode jsonNode = mapper.readTree(new URL(addr + "/lookup?topic=" + topic));
+                LogManager.getLogger(this).warn("Server connection information: " + jsonNode.toString());
+                JsonNode producers = jsonNode.get("data").get("producers");
+                for (JsonNode node : producers) {
+                    String host = node.get("broadcast_address").asText();
+                    ServerAddress address = new ServerAddress(host, node.get("tcp_port").asInt());
+                    addresses.add(address);
+                }
+            } catch (IOException e) {
+                LogManager.getLogger(this).warn("Unable to connect to address " + addr);
             }
         }
+        if (addresses.isEmpty()) {
+            throw new IOException("Unable to connect to any NSQ Lookup servers, servers tried: " + this.addresses.toString());
+        } else {
+            return addresses;
+        }
+    }
+
+    public Set<String> getLookupAddresses() {
         return addresses;
     }
 }
